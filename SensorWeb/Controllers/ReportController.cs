b3c961a6badb04e05b1f78187e790839b6d723ab
Douglas <douglima@msn.com>
2023-04-14 20:30:33 -0300

    Novo relatório.

---
+++ SensorWeb/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace SensorWeb.Controllers
+{
+    [Authorize]
+    public class ReportController : BaseController
+    {
+        IMapper _mapper;
+        private readonly IStringLocalizer<Resources.CommonResources> _localizer;
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="mapper"></param>
+        /// <param name="localizer"></param>
+        public ReportController(IMapper mapper,
+                                  IStringLocalizer<Resources.CommonResources> localizer)
+        {
+            _mapper = mapper;
+            _localizer = localizer;
+        }
+
+        // GET: ReportController
+        public ActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
